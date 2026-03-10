013b63710efa8c71e91ce9e567184b4357f2e14a
psicmiguelglez-bit <psic.miguelglez@gmail.com>
2026-03-09 20:07:56 -0700

    Update cache version from v3 to v4

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'centro-ps-v3';
+const CACHE = 'centro-ps-v4';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', function(e) {
